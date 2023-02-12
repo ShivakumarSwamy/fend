ce7c780a689f7f9e3fa717be2415e37c9d1b32ea
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-02-12 12:23:08 +0530

    initial version

--- README.md
+++ README.md
@@ -3,8 +3,18 @@
 ## Overview
 This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI.
 
-## Instructions
-This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+## Run locally
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+1. Install [nodejs](https://nodejs.org/en/) if not present
+2. Run `npm install` to install all dependencies
+3. Run `node server.js` to run application server
+4. Open http://localhost:3000 to view webpage
+
+## Insert Weather Data Based on Zipcode
+
+1. Run locally server and client as mentioned above
+2. Insert zipcode of any place in India, in "Enter India Zipcode here"
+3. Enter your feeling in "How are you feeling today?"
+4. Notice in the bottom of the page under "Most Recent Entry" that zipcode temperature in Celsius 
+   and your feeling on the weather
+
---
+++ website/app.js
@@ -0,0 +1,64 @@
+/* Global Variables */
+baseURL = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = '&appid=e69cb8bccc84bf78132076343903816a&units=metric';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.querySelector("#generate").addEventListener("click", performAction);
+
+function performAction() {
+  getWeatherDataBasedOnZipCode()
+    .then(data => postData("/newData", data))
+    .then(updateUI);
+}
+
+async function getWeatherDataBasedOnZipCode() {
+  const zipCode = document.querySelector("#zip").value;
+
+  try {
+    const response = await fetch(`${baseURL}?zip=${zipCode},in${apiKey}`);
+    return await response.json();
+  } catch(error) {
+    console.error(`Unable to get weather data for zipCode=${zipCode}, Reason:`, error);
+    throw new Error();
+  }
+}
+
+async function postData(url, data) {
+  const content = document.querySelector("#feelings").value;
+
+  try {
+    await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ temperature: data.main.temp,
+                               date: newDate,
+                               content: content
+                            })
+    });
+   } catch(error) {
+      console.error("Unable to save data, Reason:", error);
+      throw new Error();
+   }
+}
+
+async function updateUI() {
+  let allData = {};
+
+  try {
+    const response = await fetch("/all");
+    allData = await response.json();
+  } catch(error) {
+      console.error("Unable to fetch data, Reason:", error);
+      throw new Error();
+  }
+
+  document.querySelector("#date").innerHTML = allData.date;
+  document.querySelector("#temp").innerHTML = allData.temperature + " &degC";
+  document.querySelector("#content").innerHTML = allData.content;
+}
